fc03bb163a71350535e5767c9ca7e844202a65c3
rm.mansurov <rm.mansurov@tensor.ru>
2025-07-14 18:04:35 +0500

    Added solution to task with star of lecture 10

--- lection_10/task_with_star.py
+++ lection_10/task_with_star.py
@@ -6,6 +6,10 @@ import pytest
 
 
 @pytest.mark.id_check(1, 2, 3)
-def test():
-    # Здесь пишем код
-    pass
+def test(request):
+    marker = request.node.get_closest_marker("id_check")
+    if marker:
+        print(f"id_check: {', '.join(map(str, marker.args))}")
+    else:
+        print("Маркер id_check не найден")
+    assert True
